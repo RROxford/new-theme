6b773bd20bea534ee3ff3c8c66e2ea721ab35877
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2019-12-17 12:10:02 +0000

    Fixed search result URLs
    Fixed search result URLs for team and initaitives collections linking to missing pages.

--- _team/amy-orben.md
+++ _team/amy-orben.md
@@ -6,7 +6,7 @@ position: DPhil Candidate in Experimental Psychology
 image_src: amy-orben.png
 affiliations: [Experimental Psychology, Medical Sciences Division]
 groups: [ReproducibiliTea]
+
 ---
 
-Amy is a DPhil Candidate at the Department of Experimental Psychology. She is
-one of the organisers of ReprodubiliTea.
+Amy is a DPhil Candidate at the Department of Experimental Psychology. She is one of the organisers of ReprodubiliTea.
--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -9,6 +9,16 @@ var store = [
     {%- endif -%}
     {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
     {%- for doc in docs -%}
+
+      {% comment %}Here we adjust the search result URLs to be appropriate for the collection.{% endcomment %}
+      {% if doc.collection == "team" %}
+        {% capture myURL %}people/#{{ doc.title | replace: " ", "" }}{% endcapture %}
+      {% elsif doc.collection == "initiatives" %}
+        {% capture myURL %}{{ doc.collection }}/#{{ doc.title | replace: " ", "" }}{% endcapture %}
+      {% else %}
+        {% assign myURL = doc.url %}
+      {% endif %}
+
       {%- if doc.header.teaser -%}
         {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
       {%- else -%}
@@ -40,7 +50,7 @@ var store = [
           {%- endif -%}
         "categories": {{ doc.categories | jsonify }},
         "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }},
+        "url": {{ myURL | absolute_url | jsonify }},
         "teaser":
           {%- if teaser contains "://" -%}
             {{ teaser | jsonify }}
